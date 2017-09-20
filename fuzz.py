710f505db937c72d03501b81d9af5332ea181602
Yibai Zhang <xm1994@gmail.com>
2017-09-20 11:16:16 +0800

    fix timer call error.

--- fuzz.py
+++ fuzz.py
@@ -2,27 +2,33 @@ import driller
 import fuzzer
 
 import config
-
+import os
 
 class Fuzz:
     def __init__(self, binary, time_limit):
-        self.binary = binary
+        self.binary = str(binary)
         self.time_limit = time_limit
         self.fuzzer = None
+        self.started = False
 
     def start(self):
+        if self.started:
+            return
+        self.started = True
         print "[*] Enable driller..."
         self.drill_extension = driller.LocalCallback(num_workers=config.DRILLER_WORKERS)
         print "[*] Initialize fuzzer..."
         self.fuzzer = fuzzer.Fuzzer(
             self.binary, config.FUZZ_DIR, afl_count=config.FUZZER_WORKERS, force_interval=config.FUZZER_FORCE_INTERVAL,
-            create_dictionary=True, stuck_callback=self.drill_extension, time_limit=self.time_limit
+            create_dictionary=True, stuck_callback=None, time_limit=self.time_limit
         )
         print "[*] Starting fuzzer..."
         self.fuzzer.start()
 
     def kill(self):
-        self.fuzzer.kill()
+        self.started = True
+        if self.fuzzer:
+            self.fuzzer.kill()
         self.drill_extension.kill()
 
     def found_crash(self):
--- robot.py
+++ robot.py
@@ -4,30 +4,35 @@ from threading import Timer
 from multiprocessing import Pool
 import config
 import fuzz
+import logging
+import time
 
 challenges = {}
 last_round = 0
 pool = None
+logger = logging.getLogger("[robot]")
 
 
 def resolv_work():
     questions = client.get_question_status()
     for challenge in questions.CurrentChallenge:
         if not challenges.has_key(challenge.ChallengeID):
-            print "[*] Add Challenge %s, type %s." % (challenge.ChallengeID, ",".join(challenge.ChallengeType))
+            logger.info("[*] Add Challenge %s, type %s." % (challenge.ChallengeID, ",".join(challenge.ChallengeType)))
             challenges[challenge.ChallengeID] = {"challenge":challenge}
             binary_name = os.path.join(config.DOWNLOAD_DIR, str(challenge.ChallengeID))
             challenge.download(os.path.abspath(binary_name))
             fuzzer = fuzz.Fuzz(binary=binary_name, time_limit=1200)
             challenges[challenge.ChallengeID]["fuzzer"] = fuzzer
             pool.apply_async(run_fuzzer, (fuzzer,))
+            #run_fuzzer(fuzzer)
 
 
 def check_crash():
     for challengeId in challenges:
         fuzzer = challenges[challengeId]["fuzzer"]
-        if fuzzer.found_crash() :
-            pass
+        logger.info("[*]Check crash for %s" % challengeId)
+        if fuzzer.found_crash():
+            logger.info("[*]Crash for %s found!" % challengeId)
 
 
 def run_fuzzer(fuzzer):
@@ -44,12 +49,17 @@ if __name__ == "__main__":
         username = os.environ.has_key("RHG_USER") and os.environ["RHG_USER"] or "team_11"
         password = os.environ.has_key("RHG_PASS") and os.environ["RHG_PASS"] or "wz2mrzmc"
         client = rhgapi.RHGClient(username, password)
-        resolver = Timer(60, resolv_work())
+        resolv_work()
+        resolver = Timer(60, resolv_work)
         resolver.start()
-        crasher = Timer(60, check_crash())
+        crasher = Timer(60, check_crash)
         crasher.start()
+        while True:
+            time.sleep(65535)
     except KeyboardInterrupt:
         pool.close()
+        for challenge_id in challenges:
+            challenges[challenge_id]["fuzzer"].stop()
         pool.join()
 
 
